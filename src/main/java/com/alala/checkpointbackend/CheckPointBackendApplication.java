6d7301b58b97e671cb2139be7933f1cf69f6d898
李松霖 <sunglin@lisonglindeMacBook-Pro.local>
2025-08-05 17:33:34 +0800

    init

---
+++ src/main/java/com/alala/checkpointbackend/CheckPointBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alala.checkpointbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckPointBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckPointBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alala/checkpointbackend/CheckPointBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alala.checkpointbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckPointBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
