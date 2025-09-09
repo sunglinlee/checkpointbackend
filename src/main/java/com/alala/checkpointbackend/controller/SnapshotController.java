378c0d10a0cc00300c7e380d15c84f9edb062c77
李松霖 <sunglin@lisonglindeMacBook-Pro.local>
2025-09-09 16:54:34 +0800

    test

--- src/main/java/com/alala/checkpointbackend/controller/SnapshotController.java
+++ src/main/java/com/alala/checkpointbackend/controller/SnapshotController.java
@@ -7,10 +7,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "snapshot", description = "快照相關API")
 @RestController
@@ -20,7 +17,7 @@ public class SnapshotController {
     private final QuestionnaireService questionnaireService;
 
     @Operation(description = "查詢全部問卷")
-    @GetMapping(value = "/snapshots")
+    @PostMapping(value = "/snapshots")
     public BaseResponse<String> snapshots(@RequestBody String email) throws JsonProcessingException {
         return new BaseResponse<>(StatusCode.SUCCESS.getCode(), questionnaireService.query(email));
     }
